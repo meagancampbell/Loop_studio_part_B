c69487664329be6cc014a3d439cee8d4755ea323
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-20 21:06:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Construct a for loop that combines two strings together, alternating the characters from each source.  Print the result.
+
+//Example: If string = “1234” and otherString = “5678”, then the output will be “15263748”.
+
+//For now, make sure the two strings contain the same number of characters!
+
+let code1 = ('Rutabagas'.toLowerCase());
+let code2 = ('Chemistry'.toLowerCase());
+let newString = ''; //the combo of the two
+console.log(code1);
+console.log(code2);
+console.log(strLength1 = code1.length);
+console.log(strLength2 = code2.length);
+
+newString = code1;
+
+if Chemistry.length === Rutabagas.length){
+  let newLength = code1.length; // why is new length the same as Rutabagas?
+}else (let i = 0; i < newLength; i++){
+  newString = newString+Chemistry[i]+Rutabagas[i];
+}
+console.log(newString);
